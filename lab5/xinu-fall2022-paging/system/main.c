304673dcfa4ac25fc8ab1c1ca5850ac80cf5751e
Suraj Aralihalli <suraj.ara16@gmail.com>
2022-11-08 19:40:31 -0500

    initial commit

---
+++ lab5/xinu-fall2022-paging/system/main.c
@@ -0,0 +1,21 @@
+/*  main.c  - main */
+
+#include <xinu.h>
+#include <stdio.h>
+
+process	main(void)
+{
+
+	/* Start the network */
+	/* DO NOT REMOVE OR COMMENT THIS CALL */
+	netstart();
+
+	/* Initialize the page server */
+	/* DO NOT REMOVE OR COMMENT THIS CALL */
+	// psinit();
+
+	// Insert test code below
+
+	kprintf("Hello World\n");
+	return OK;
+}
